eb6bb95fbba42f8099d17952b103a76bddfce13e
Andreas <andy.reitberger@gmail.com>
2022-01-04 14:18:28 +0100

    Update to 1.0.7
    - Added: Global interface for RestApiClients
    - Added: StackToPath converter

--- source/CoreSharp/Utilities/CommonConverters.cs
+++ source/CoreSharp/Utilities/CommonConverters.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace AndreasReitberger.Core.Utilities
 {
@@ -13,6 +15,20 @@ namespace AndreasReitberger.Core.Utilities
                 list.AddRange(pairs.Value);
             }
             return list;
+        }
+
+        public static string StackToPath(Stack<string> stack, string separator)
+        {
+            StringBuilder sb = new();
+            for (int i = stack.Count - 1; i >= 0; i--)
+            {
+                sb.Append(stack.ElementAt(i));
+                if (i > 0)
+                {
+                    sb.Append(separator);
+                }
+            }
+            return sb.ToString();
         }
     }
 }
